2d8ca5ffabdc828a68c2dc0584bdab2c92f4ffa1
Agustin Carrasco <asermax@gmail.com>
2016-10-30 13:49:53 -0300

    docker: further improve image creation to include dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM asermax/node-yarn
 MAINTAINER agustin carrasco <asermax@gmail.com>
 
+WORKDIR /opt/app
+
 COPY dist /opt/app/dist
 COPY package.json yarn.lock index.js /opt/app/
-VOLUME /opt/apt/node_modules
+RUN yarn install --ignore-optional --production && yarn cache clean
 
-WORKDIR /opt/app
-ENV NODE_ENV production
 EXPOSE 8000
 
 ENTRYPOINT ["yarn"]
--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-docker-compose -f docker-compose-build.yml run --rm build
-docker build -t asermax/plus .
+
+# generate dist
+docker-compose --file docker-compose-build.yml run --rm build
+
+# WORKAROUND: yarn doesn't work correctly with the --production flag
+# we need to remove dev dependencies before building the image
+cp package.json package.json.bak
+jq 'del(.devDependencies)' package.json.bak | tee package.json > /dev/null
+
+# remove old image and build
+docker rmi --force asermax/plus
+docker build --tag asermax/plus .
+
+# restore package.json
+mv package.json.bak package.json
